6c7927ee32d6960a449025ae5aeefb5a96654caa
Pauline Gaudet-Chardonnet <pauline.gaudet-chardonnet@lip6.fr>
2018-08-15 14:10:47 +0200

    settings update

--- browser_bro/settings.py
+++ browser_bro/settings.py
@@ -162,3 +162,5 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Africa/Nairobi'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
