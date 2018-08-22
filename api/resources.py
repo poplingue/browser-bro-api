8223c7568d24629c86f811308a27f77b5f52059a
Pauline Gaudet-Chardonnet <pauline.gaudet-chardonnet@lip6.fr>
2018-08-22 13:42:10 +0200

    authorize POST

--- api/resources.py
+++ api/resources.py
@@ -1,7 +1,9 @@
 from tastypie.resources import ModelResource
 from api.models import Custom
+from tastypie.authorization import Authorization
 
 class CustomResource(ModelResource):
     class Meta:
         queryset = Custom.objects.all()
         resource_name = 'customs'
+        authorization = Authorization()
